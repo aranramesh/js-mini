2c00f48816e9de83ef8927a1dd486dc4f8db8840
aranramesh <svramesh692@gmail.com>
2022-10-16 11:25:41 +0530

    port

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+// let click = document.getElementById('click');
+// let ins = document.getElementById('ins');
+// let p = ass;
+// function nana (){
+  
+    
+
+// }
+
+let cities =[{a:'tenkasi',b:'nellai',c:'madurai',d:'chrnnai',e:'kovai'}];
+
+let count = cities.length;
+
+// for( i = 0 ; i< count ; i++){
+//     console.log(cities [i]);
+// };
+
+for( x of cities){
+  console.log(Object.keys.cities.find(tenkasi));
+}
